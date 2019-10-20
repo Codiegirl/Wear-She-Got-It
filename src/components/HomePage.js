41e11db3ba64ffdb2a81cf17969a7b8efae33279
github username <github email address>
2019-10-20 11:19:30 -0500

    photos on homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import travel from "../images/travel.jpg"
+import health from "../images/health.jpg"
 
 export default class HomePage extends React.Component {
     render(){
         return(
             <div>
-            <h1>Welcome</h1>
+            <h1>Where She Got It</h1>
+            <img style={{width: 300, height: 300 }}src={travel} alt=""></img> <img style={{width: 300, height: 300 }}src={health} alt=""></img>
             
             </div>
         )

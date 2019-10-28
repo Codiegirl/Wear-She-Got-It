a323e01be8b1e441bff465bb180c97cc8d4662e5
github username <github email address>
2019-10-28 10:51:58 -0500

    trying to get navbar lines to show

---
+++ src/components/DrawerToggleButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default class DrawerToggleButton extends React.Component {
+    render(){
+        return(
+
+    <div>
+    <button className="toggle-button">
+        <div className="toggle-button-line"/>
+        <div className="toggle-button-line"/>
+        <div className="toggle-button-line"/>
+    </button>
+    </div>
+    
+);
+        }
+    }
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import DrawerToggleButton from './DrawerToggleButton'
 
 
 
-
-
-const NavBar = () => (
-    <header className="navbar-container">
+export default class NavBar extends React.Component {
+    render(){
+        return(
+        <header className="navbar-container">
         <nav className="navbar-navigation">
-            <div></div>
+            <div>
+                <DrawerToggleButton />
+            </div>
             <div className="navbar-logo"><a href="/">Where She Got It</a></div>
             <div className="spacer"/>{/*to create space between the logo and links*/}
             <div className="navbar-navigation-links">
@@ -33,28 +36,30 @@ const NavBar = () => (
         </nav>
     </header>
     
-);
+
+        );
+    }
+}
 
 
-export default NavBar;
 
 
-{/* <nav className="navbar-container">
-        <ul className="navbar-links">
-            <li>
-                <Link to="/homepage" className="navbar-links">Where She Got It</Link>
-            </li>
-            <li>
-                <Link to="/travel" className="navbar-links">Travel</Link>
-            </li>
-            <li>
-                <Link to="/health" className="navbar-links">Health</Link>
-            </li>
-            <li>
-                <Link to="/fashion" className="navbar-links">Fashion</Link>
-            </li>
-            <li>
-                <Link to="/decor" className="navbar-links">Decor</Link>
-            </li>
-        </ul>
-        </nav>*/}
+//  <nav className="navbar-container">
+//         <ul className="navbar-links">
+//             <li>
+//                 <Link to="/homepage" className="navbar-links">Where She Got It</Link>
+//             </li>
+//             <li>
+//                 <Link to="/travel" className="navbar-links">Travel</Link>
+//             </li>
+//             <li>
+//                 <Link to="/health" className="navbar-links">Health</Link>
+//             </li>
+//             <li>
+//                 <Link to="/fashion" className="navbar-links">Fashion</Link>
+//             </li>
+//             <li>
+//                 <Link to="/decor" className="navbar-links">Decor</Link>
+//             </li>
+//         </ul>
+//         </nav>
